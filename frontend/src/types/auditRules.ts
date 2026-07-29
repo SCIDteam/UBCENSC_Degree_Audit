453e92c1456aaada8e7eb9de1e6f9b8f065c2868
Ritisha Sharma <ritishas2000@gmail.com>
2026-07-29 15:45:08 -0700

    Add auditrules types

---
+++ frontend/src/types/auditRules.ts
@@ -0,0 +1,56 @@
+export type Unit = 'credits' | 'categories' | 'course'
+export type Metric = (
+    'required_course' | 
+    'eligible_course' | 
+
+    'min_total_credits' |
+    'min_science_credits' |
+    'min_arts_credits' |
+    'min_communication_credits' |
+    'max_other_faculty_credits' |
+
+    'min_upper_level_credits' |
+    'min_upper_level_science_credits' |
+    'min_science_credits_at_level' |
+    'min_science_credits_at_or_above_level'|
+
+    'min_breadth_categories' |
+    'one_course_from_list' |
+    'one_lab_course' 
+)
+export type ProgramContext = 'All' | 'MajorOrHonours' | 'Major' | 'Honours'
+
+export type BaseRule = {
+    metric: Metric
+    value: number
+    unit: Unit
+    notes: string
+}
+
+export interface FacultyRequirements extends BaseRule {
+    id: string
+    program_context: ProgramContext
+}
+
+export interface CourseRules extends BaseRule {
+    id: string
+    program_context: ProgramContext
+    calendar_year: string
+    program_type: string
+    course_code: string
+}
+
+export interface PromotionRules extends BaseRule {
+    id: string
+    promotion_to: number
+    course_level_min: string
+    course_level_max: string
+    science_only: boolean
+}
+
+export interface CourseRequirements extends BaseRule {
+    id: string
+    program: string
+    program_context: ProgramContext
+    calendar_year: string
+}
